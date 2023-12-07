6776efca6c060712b0d825f23a41b89e6c397239
Dsuar <suarezdanieljo@gmail.com>
2023-12-07 10:36:53 -0400

    modificacion interfaz main.py

--- main.py
+++ main.py
@@ -10,10 +10,25 @@ warnings.filterwarnings("ignore")
 
 modelo = joblib.load("./modelo/modelo_ml.pkl")
 
+# clase 'Regular' por defualt
 class1 = 0
 
 st.title('Predicción de Probabilidad de Éxito de un Nuevo Restaurant')
 st.markdown('***')
+# ---------------------------------------------------------------
+# eleccion de categoria
+
+# Recupere todos los datos de la tabla en un DataFrame de pandas
+df = pd.read_csv('./modelo/atributos_csv/category.csv')
+
+# Crear una lista desplegable con los valores de 'category'
+cat = st.sidebar.selectbox(
+    'Selecciona una Categoria',
+    df['category'])
+
+category = df[df['category'] == cat]['category_int'].values[0]
+
+# st.write(category)
 # ---------------------------------------------------------------
 
 st.sidebar.markdown('## Cuántas Estrellas? Dónde? ')
@@ -37,23 +52,9 @@ rating = options[selected_option]
 
 # st.write(rating)
 # ---------------------------------------------------------------
-# eleccion de categoria
-
-df = pd.read_csv('./modelo/atributos_csv/category.csv')
-
-# Crear una lista desplegable con los valores de 'category'
-cat = st.selectbox(
-    'Selecciona una Categoria',
-    df['category'])
-
-st.write('Has seleccionado:', cat)
-
-category = df[df['category'] == cat]['category_int'].values[0]
-
-# st.write(category)
-# ---------------------------------------------------------------
 # eleccion de estado
 
+# Recupere todos los datos de la tabla en un DataFrame de pandas
 df = pd.read_csv('./modelo/atributos_csv/estados.csv')
 
 # Crear un conjunto de botones de opción con los valores de 'state'
@@ -67,53 +68,71 @@ state = df[df['state'] == sta]['state_int'].values[0]
 
 # st.write(state)
 # ---------------------------------------------------------------
+st.write('## Restaurante de Categoría', cat, 'en el Estado de', sta)
+st.markdown('***')
+# ---------------------------------------------------------------
+st.markdown('### Selección de Servicios:')
 # eleccion de servicio 0
 
-df = pd.read_csv('./modelo/atributos_csv/Service0.csv')
+# Recupere todos los datos de la tabla en un DataFrame de pandas
+df1 = pd.read_csv('./modelo/atributos_csv/Service0.csv')
 
-# Crear una lista desplegable con los valores de 'servicio'
-Ser0 = st.selectbox(
-    'Servicio 1 a ofrecer',
-    df['Service0'])
-
-st.write('Has seleccionado:', Ser0)
-
-Service0 = df[df['Service0'] == Ser0]['Service0_int'].values[0]
-
-# st.write(Service0)
 # ---------------------------------------------------------------
 # eleccion de servicio 1
 
-df = pd.read_csv('./modelo/atributos_csv/Service1.csv')
+# Recupere todos los datos de la tabla en un DataFrame de pandas
+df2 = pd.read_csv('./modelo/atributos_csv/Service1.csv')
 
-# Crear una lista desplegable con los valores de 'servicio'
-Ser1 = st.selectbox(
-    'Servicio 2 a ofrecer',
-    df['Service1'])
-
-st.write('Has seleccionado:', Ser1)
-
-Service1 = df[df['Service1'] == Ser1]['Service1_int'].values[0]
-
-# st.write(Service1)
 # ---------------------------------------------------------------
 # eleccion de servicio 2
 
-df = pd.read_csv('./modelo/atributos_csv/Service2.csv')
+# Recupere todos los datos de la tabla en un DataFrame de pandas
+df3 = pd.read_csv('./modelo/atributos_csv/Service2.csv')
 
-# Crear una lista desplegable con los valores de 'servicio'
-Ser2 = st.selectbox(
-    'Servicio 3 a ofrecer',
-    df['Service2'])
+# Divide la pantalla en tres columnas
+col1, col2, col3 = st.columns(3)
 
-st.write('Has seleccionado:', Ser2)
+# Agrega una lista desplegable en cada columna
+# Crear una lista desplegable con los valores de 'servicio 1'
+with col1:
+    Ser0 = st.selectbox(
+        'Servicio 1',
+        df1['Service0'])
 
-Service2 = df[df['Service2'] == Ser2]['Service2_int'].values[0]
+    # st.write('Has seleccionado:', Ser0)
 
-# st.write(Service2)
+    Service0 = df1[df1['Service0'] == Ser0]['Service0_int'].values[0]
+
+    # st.write(Service0)
+
+# Crear una lista desplegable con los valores de 'servicio 2'
+with col2:
+    Ser1 = st.selectbox(
+        'Servicio 2',
+        df2['Service1'])
+
+    # st.write('Has seleccionado:', Ser1)
+
+    Service1 = df2[df2['Service1'] == Ser1]['Service1_int'].values[0]
+
+    # st.write(Service1)
+
+# Crear una lista desplegable con los valores de 'servicio 3'
+with col3:
+    Ser2 = st.selectbox(
+        'Servicio 3',
+        df3['Service2'])
+
+    # st.write('Has seleccionado:', Ser2)
+
+    Service2 = df3[df3['Service2'] == Ser2]['Service2_int'].values[0]
+    # st.write(Service2)
+
 # ---------------------------------------------------------------
+st.markdown('### Selección de Acceso Preferencial:')
 # eleccion de Accecibilidad
 
+# Recupere todos los datos de la tabla en un DataFrame de pandas
 df = pd.read_csv('./modelo/atributos_csv/Accessibilidad.csv')
 
 # Crear una lista desplegable con los valores de 'Accecibilidad'
@@ -121,115 +140,140 @@ Acc = st.selectbox(
     'Seleccione una opción',
     df['Accessibility0'])
 
-st.write('Has seleccionado:', Acc)
+# st.write('Has seleccionado:', Acc)
 
 Accessibility0 = df[df['Accessibility0']
                     == Acc]['Accessibility0_int'].values[0]
 
 # st.write(Accessibility0)
 # ---------------------------------------------------------------
+st.markdown('### Selección de Ofrecimientos:')
 # eleccion de Ofertas
 
-df = pd.read_csv('./modelo/atributos_csv/Offerings0.csv')
-
-# Crear una lista desplegable con los valores de 'Ofertas'
-Off0 = st.selectbox(
-    'Seleccione una Oferta 1',
-    df['Offerings0'])
-
-st.write('Has seleccionado:', Off0)
-
-Offerings0 = df[df['Offerings0']
-                == Off0]['Offerings0_int'].values[0]
-
-# st.write(Offerings0)
+# Recupere todos los datos de la tabla en un DataFrame de pandas
+df4 = pd.read_csv('./modelo/atributos_csv/Offerings0.csv')
 # ---------------------------------------------------------------
 # eleccion de Ofertas
 
-df = pd.read_csv('./modelo/atributos_csv/Offerings1.csv')
-
-# Crear una lista desplegable con los valores de 'Ofertas'
-Off1 = st.selectbox(
-    'Seleccione una Oferta 2',
-    df['Offerings1'])
-
-st.write('Has seleccionado:', Off1)
-
-Offerings1 = df[df['Offerings1']
-                == Off1]['Offerings1_int'].values[0]
-
-# st.write(Offerings1)
+# Recupere todos los datos de la tabla en un DataFrame de pandas
+df5 = pd.read_csv('./modelo/atributos_csv/Offerings1.csv')
 # ---------------------------------------------------------------
 # eleccion de Ofertas
 
-df = pd.read_csv('./modelo/atributos_csv/Offerings2.csv')
+# Recupere todos los datos de la tabla en un DataFrame de pandas
+df6 = pd.read_csv('./modelo/atributos_csv/Offerings2.csv')
 
 # Crear una lista desplegable con los valores de 'Ofertas'
-Off2 = st.selectbox(
-    'Seleccione una Oferta 3',
-    df['Offerings2'])
 
-st.write('Has seleccionado:', Off2)
+# Divide la pantalla en tres columnas
+col4, col5, col6 = st.columns(3)
 
-Offerings2 = df[df['Offerings2']
-                == Off2]['Offerings2_int'].values[0]
+# Agrega una lista desplegable en cada columna
+# Crear una lista desplegable con los valores de 'Ofertas'
+with col4:
+    Off0 = st.selectbox(
+        'Oferta 1',
+        df4['Offerings0'])
 
-# st.write(Offerings2)
+    # st.write('Has seleccionado:', Off0)
+
+    Offerings0 = df4[df4['Offerings0']
+                     == Off0]['Offerings0_int'].values[0]
+
+    # st.write(Offerings0)
+
+# Crear una lista desplegable con los valores de 'Ofertas'
+with col5:
+
+    Off1 = st.selectbox(
+        'Oferta 2',
+        df5['Offerings1'])
+
+    # st.write('Has seleccionado:', Off1)
+
+    Offerings1 = df5[df5['Offerings1']
+                     == Off1]['Offerings1_int'].values[0]
+
+    # st.write(Offerings1)
+
+# Crear una lista desplegable con los valores de 'servicio 3'
+with col6:
+    Off2 = st.selectbox(
+        'Oferta 3',
+        df6['Offerings2'])
+
+    # st.write('Has seleccionado:', Off2)
+
+    Offerings2 = df6[df6['Offerings2']
+                     == Off2]['Offerings2_int'].values[0]
+
+    # st.write(Offerings2)
 # ---------------------------------------------------------------
+st.markdown('### Selección de Amenidad, Tipo de Atmosfera y Tipo de Público:')
 # eleccion de Amenidades
 
-df = pd.read_csv('./modelo/atributos_csv/Amenities0.csv')
-
-# Crear una lista desplegable con los valores de 'Amenidades'
-Ame = st.selectbox(
-    'Seleccione una Amenidad',
-    df['Amenities0'])
-
-st.write('Has seleccionado:', Ame)
-
-Amenities0 = df[df['Amenities0']
-                == Ame]['Amenities0_int'].values[0]
-
-# st.write(Amenities0)
+# Recupere todos los datos de la tabla en un DataFrame de pandas
+df7 = pd.read_csv('./modelo/atributos_csv/Amenities0.csv')
 # ---------------------------------------------------------------
 # eleccion de Atmosfera
-
-df = pd.read_csv('./modelo/atributos_csv/Atmosphere0.csv')
-
-# Crear una lista desplegable con los valores de 'Atmosferas'
-Atm = st.selectbox(
-    'Seleccione una Atmósfera',
-    df['Atmosphere0'])
-
-st.write('Has seleccionado:', Atm)
-
-Atmosphere0 = df[df['Atmosphere0']
-                 == Atm]['Atmosphere0_int'].values[0]
-
-# st.write(Atmosphere0)
+# Recupere todos los datos de la tabla en un DataFrame de pandas
+df8 = pd.read_csv('./modelo/atributos_csv/Atmosphere0.csv')
 # ---------------------------------------------------------------
 # eleccion de Enfoque
 
-df = pd.read_csv('./modelo/atributos_csv/Crowd0.csv')
+# Recupere todos los datos de la tabla en un DataFrame de pandas
+df9 = pd.read_csv('./modelo/atributos_csv/Crowd0.csv')
+
+# Divide la pantalla en tres columnas
+col7, col8, col9 = st.columns(3)
+
+# Agrega una lista desplegable en cada columna
+# Crear una lista desplegable con los valores de 'Amenidades'
+with col7:
+    Ame = st.selectbox(
+        'Amenidad',
+        df7['Amenities0'])
+
+    # st.write('Has seleccionado:', Ame)
+
+    Amenities0 = df7[df7['Amenities0']
+                     == Ame]['Amenities0_int'].values[0]
+
+# st.write(Amenities0)
+
+# Crear una lista desplegable con los valores de 'Atmosferas'
+with col8:
+    Atm = st.selectbox(
+        'Atmósfera',
+        df8['Atmosphere0'])
+
+    # st.write('Has seleccionado:', Atm)
+
+    Atmosphere0 = df8[df8['Atmosphere0']
+                      == Atm]['Atmosphere0_int'].values[0]
+
+    # st.write(Atmosphere0)
 
 # Crear una lista desplegable con los valores de 'Enfoques'
-Cro = st.selectbox(
-    'Seleccione un Público',
-    df['Crowd0'])
+with col9:
+    Cro = st.selectbox(
+        'Público',
+        df9['Crowd0'])
 
-st.write('Has seleccionado:', Cro)
+    # st.write('Has seleccionado:', Cro)
 
-Crowd0 = df[df['Crowd0']
-            == Cro]['Crowd0_int'].values[0]
+    Crowd0 = df9[df9['Crowd0']
+                 == Cro]['Crowd0_int'].values[0]
 
-# st.write(Crowd0)
+    # st.write(Crowd0)
 
 
-# Supongamos que tienes el siguiente DataFrame
+# sistema de clasificacion con árbol de decisión para determinar que clase es el restaurant
+# tienes el siguiente DataFrame
 x_prueba = pd.DataFrame(columns=('rating', 'category_int', 'state_int', 'Service0_int', 'Service1_int', 'Service2_int', 'Accessibility0_int', 'Offerings0_int',
                                  'Offerings1_int', 'Offerings2_int', 'Amenities0_int', 'Atmosphere0_int', 'Crowd0_int', 'class1'))
 
-# Supongamos que tienes los siguientes datos
+# tienes los siguientes datos
 datos = (rating, category, state, Service0, Service1, Service2, Accessibility0, Offerings0, Offerings1, Offerings2, Amenities0,
          Atmosphere0, Crowd0, class1)
 
@@ -247,31 +291,47 @@ st.markdown("## Con una probabilidad de Acierto de: " +
 st.markdown('***')
 # ---------------------------------------------------------------
 st.markdown('### A continuacion te invitamos a explorar los casos de exitos de acuardo a la categoria y el estado que has escogido, puedes elegir cualquier opcion para ver sus atributos mas llamativos.')
+
 # eleccion de Restaurant de Referencia
+
+# Recupere todos los datos de la tabla en un DataFrame de pandas
 data_details = pd.read_csv('./modelo/category_details.csv')
+
 data_details = data_details[(data_details['category_det'] == cat) & (
     data_details['state'] == sta)]
 
-# Crear una lista desplegable con los valores de 'name_y'
-nombre = st.selectbox(
-    'Selecciona un Restaurante de Referencia',
-    data_details['name_y'])
+try:
+    nombre = st.selectbox(
+        'Selecciona un Restaurante de Referencia',
+        data_details['name_y'])
+    st.write('Has seleccionado:', nombre)
 
-st.write('Has seleccionado:', nombre)
+    if not data_details[data_details['name_y'] == nombre].empty:
+        caracteristicas = data_details.loc[data_details['name_y']
+                                           == nombre, 'features'].values[0]
+    else:
+        st.write('Para el estado de ' + sta +
+                 ' no existen lugares de categoria ' + cat + ', que sean de "Clase Premium", lo sentimos.')
+        caracteristicas = None
+except Exception:
+    st.write('Ha ocurrido un error al obtener las características.')
 
 caracteristicas = data_details.loc[data_details['name_y']
                                    == nombre, 'features'].values[0]
 
-
 st.markdown(" El Restaurant de Referencia: " + nombre + ", esta ubicado en el Estado de " + sta +
             " y es de la categoría de " + cat + " el mismo es de clase Premiun y cuenta con las siguientes caracteristicas: " + caracteristicas)
+
 # ---------------------------------------------------------------
+# sistema de recomendacion por similitud de coseno
+category1 = cat + ' - ' + sta + ' - ' + nombre
+
 # se carga los datasets que se va a utilizar para dos dataframes distintos
 
-category1 = cat + ' - ' + sta + ' - ' + nombre
-print(category1)
-
+# Recupere todos los datos de la tabla en un DataFrame de pandas
 data = pd.read_csv('./modelo/category_features.csv')
+
+# Recupere todos los datos de la tabla en un DataFrame de pandas
 data_det = pd.read_csv('./modelo/category_details.csv')
 
 # crear una matriz de características de los juegos
@@ -292,8 +352,18 @@ rest_simil = cos_sim_df.loc[category1]
 simil_ordenada = rest_simil.sort_values(ascending=False)
 resultado = simil_ordenada.head(6).reset_index()
 
-result_df = resultado.merge(data_det, on='category', how='left')
-result_df = result_df[['name_y', 'state', 'features']][1:6]
+if not resultado.empty:
+    result_df = resultado.merge(data_det, on='category', how='left')
+    result_df = result_df[['name_y', 'category_det', 'state', 'features']][1:6]
 
-st.markdown(" A demas, sugerimos que se observen con atencion los casos de exitos garantizados similares a las caracteristicas que has escogido: ")
-st.dataframe(result_df)
+    try:
+        st.markdown(
+            " A demas, sugerimos que se observen con atencion los casos de exitos garantizados similares a las caracteristicas que has escogido: ")
+        st.dataframe(result_df)
+
+    except Exception as e:
+        st.write('Lo siento, este para el estado de' + sta +
+                 'no existe restaurante de categoria ' + cat, str(e))
+
+else:
+    st.write('No se encontraron resultados.')
